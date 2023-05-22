4aa67f732d0f7e81104332f347e9ccd56ebd119c
Szymon <szymonkadelski@gmai.com>
2023-05-23 00:32:55 +0200

    Refactor

--- resources/views/games/list.blade.php
+++ resources/views/games/list.blade.php
@@ -2,34 +2,34 @@
 
 @section('content')
 
-        <div class="row mt-3">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fas fa-table mr-1"></i> Gry
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-bordered" id="dataTable">
-                            <thead>
-                                <th>Lp</th>
-                                <th>Title</th>
-                                <th>Score</th>
-                                <th>Category</th>
-                                <th>Options</th>
-                            </thead>
-    @foreach ($movies as $user)
+    <div class="row mt-3">
+        <div class="card">
+            <div class="card-header">
+                <i class="fas fa-table mr-1"></i> Gry
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable">
+                        <thead>
+                        <th>Lp</th>
+                        <th>Title</th>
+                        <th>Score</th>
+                        <th>Category</th>
+                        <th>Options</th>
+                        </thead>
+                        @foreach ($movies as $user)
 
-            <tr>
-                <td>{{ $user->id }}</td> <td>{{ $user->title }}</td>
-                <td>{{ $user->score??'?' }}/10</td> <td>{{ $user->genre_id }}</td>
-                <td>
-                    <a href="{{route('get.game',['id'=>$user->id])}}">Show</a>
-                </td>
-            </tr>
-    @endforeach
-                        </table>
-                    </div>
+                            <tr>
+                                <td>{{ $user->id }}</td> <td>{{ $user->title }}</td>
+                                <td>{{ $user->score??'?' }}/10</td> <td>{{ $user->genre_id }}</td>
+                                <td>
+                                    <a href="{{route('get.game',['id'=>$user->id])}}">Show</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </table>
                 </div>
             </div>
-    </ul>
+        </div>
+        </ul>
 @endsection
